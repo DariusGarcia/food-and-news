e3c2cfe4d936894241fd741ffc14a28f536b8dfa
Darius Garcia <dariusgarcia888@gmail.com>
2022-11-25 11:52:27 -0800

    add comments to JS file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,14 @@
 var tastyURL = `https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=under_30_minutes`
+// json data for dev fetch calls instead of using up the limited API calls from Tasty API.
 var tastyDev = './data.json'
 
 // query select elements
 var recipeContainerEl = document.querySelector('#recipeContainer')
+var recipeDropdown = document.querySelector('#recipeDropdown')
+var buttonEl = document.querySelector('#searchBtn')
+var recipeDetailsContainerEl = document.querySelector(
+	'#recipe-details-container'
+)
 
 const options = {
 	method: 'GET',
@@ -12,6 +18,7 @@ const options = {
 	},
 }
 
+// fetch Tasty API to get recipe details
 function fetchRecipe() {
 	fetch(tastyURL, options)
 		.then((response) => response.json())
@@ -23,18 +30,28 @@ function fetchRecipe() {
 		.catch((err) => console.error(err))
 }
 
-var recipeDropdown = document.querySelector('#recipeDropdown')
+// function that populates the dropdown menu with recipe results
 function handleResults(arr) {
 	for (var i = 0; i < arr.length; i++) {
 		var dropdownItem = document.createElement('option')
 		dropdownItem.textContent = arr[i].name
+		dropdownItem.addEventListener('change', handleChange(arr, i))
 		recipeDropdown.appendChild(dropdownItem)
 	}
 	return recipeDropdown
 }
 
-var buttonEl = document.querySelector('#searchBtn')
-buttonEl.addEventListener('click', () => fetchRecipe())
+// populates the recipe card with details
+function handleChange(object, i) {
+	var randomEl = document.createElement('div')
+	randomEl.textContent = object[i].description
+	recipeDetailsContainerEl.appendChild(randomEl)
+	console.log('hi')
+	return recipeDetailsContainerEl
+}
+
+buttonEl.addEventListener('click', fetchRecipe())
+fetchRecipe()
 
 // object properties that we are using to display as data
 // console.log(arr[i].name)
