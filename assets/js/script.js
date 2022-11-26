cfd41f8b7dcc3e1add610e389ef5991645ba78c8
Darius Garcia <dariusgarcia888@gmail.com>
2022-11-26 02:38:35 -0800

    populate recipe card with calories and images

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,7 @@
 var tastyURL = `https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=under_30_minutes`
 
-// json data for dev fetch calls instead of using up the limited API calls from Tasty API.
-var tastyDev = './data.json'
-
 // edamam api
+// new API endpoint because the old API hit the monthly 500 limit
 var appID = '8909c56f'
 var appAPIKey = '77ee9383a58d5bfa72e049c92b170546'
 
@@ -12,14 +10,19 @@ var recipeContainerEl = document.querySelector('#recipeContainer')
 var buttonEl = document.querySelector('#searchBtn')
 var recipeSearchBtn = document.querySelector('#recipe-search-btn')
 var recipeContentCardEl = document.querySelector('#recipe-content-card')
+var recipeDescriptionContainerEl = document.querySelector(
+	'#recipe-description-container'
+)
 var recipeDetailsContainerEl = document.querySelector(
 	'#recipe-details-container'
 )
 
+var edamamDataStore = []
 // fetch Edamam API endpoint to get recipe details (10,000 calls/ month limit)s
 function fetchEdamam(event) {
 	event.preventDefault()
-	recipeContentCardEl.innerHTML = ''
+	// reset the data array to be empty
+	edamamDataStore.length = 0
 	var userRecipeSearchInputEl = document.querySelector(
 		'#recipe-search-input'
 	).value
@@ -28,17 +31,65 @@ function fetchEdamam(event) {
 		.then((response) => response.json())
 		.then((data) => {
 			const dataReceived = data.hits
+			console.log(`call data: ${JSON.stringify(dataReceived)}`)
+			edamamDataStore.push(JSON.stringify(dataReceived))
+			console.log(`Populated array: ${JSON.stringify(edamamDataStore[0][1])}`)
 			handleRecipeResults(dataReceived)
 			return data.hits
 		})
+	userRecipeSearchInputEl.textContent = ''
 }
 
 function handleRecipeResults(arr) {
 	for (var i = 0; i < arr.length; i++) {
-		var recipeItem = document.createElement('h3')
-		recipeItem.textContent = arr[i].recipe.label
-		recipeContentCardEl.appendChild(recipeItem)
+		var cardContainer = document.createElement('article')
+		cardContainer.setAttribute(
+			'class',
+			' flex flex-col gap-2 w-full bg-white p-2 rounded-md my-2 hover:bg-blue-200'
+		)
+		var recipeLabelEl = document.createElement('h3')
+		recipeLabelEl.setAttribute(
+			'class',
+			'flex flex-col  p-2  rounded-md cursor-pointer '
+		)
+		recipeLabelEl.textContent = arr[i].recipe.label
+		var recipeCaloriesEl = document.createElement('p')
+		recipeCaloriesEl.setAttribute(
+			'class',
+			'flex flex-col  p-2  rounded-md cursor-pointer '
+		)
+		recipeCaloriesEl.textContent = `Calories: ${arr[i].recipe.calories.toFixed(
+			2
+		)}`
+		var recipeImageEl = document.createElement('img')
+		recipeImageEl.setAttribute('src', arr[i].recipe.image)
+		recipeImageEl.setAttribute('id', 'recipe-image')
+		cardContainer.appendChild(recipeLabelEl)
+		cardContainer.appendChild(recipeCaloriesEl)
+		cardContainer.appendChild(recipeImageEl)
+		// recipeItem.addEventListener('click', displayRecipeData(arr, i))
+		recipeContentCardEl.appendChild(cardContainer)
+		console.log(`description: ${edamamDataStore[0][1].label}`)
 	}
+	return recipeContentCardEl
+}
+
+function displayRecipeData(arr, i) {
+	var selectedRecipe = arr[i].recipe.label
+	var detailsURL = `https://api.edamam.com/api/recipes/v2?type=public&q=${selectedRecipe}&app_id=${appID}&app_key=${appAPIKey}`
+	fetch(detailsURL)
+		.then((response) => response.json())
+		.then((data) => {
+			var dataReceived = data.hits
+			return dataReceived
+		})
+
+	for (var i = 0; i < dataReceived.length; i++) {
+		var recipeDescriptionItem = document.createElement('p')
+		recipeDescriptionItem.textContent = dataReceived[i].recipe.label
+		recipeDescriptionContainerEl.appendChild(recipeDescriptionItem)
+	}
+	return recipeDescriptionContainerEl
 }
 
 recipeSearchBtn.addEventListener('click', fetchEdamam)
@@ -51,6 +102,15 @@ const options = {
 		'X-RapidAPI-Host': 'tasty.p.rapidapi.com',
 	},
 }
+
+// recipe.dietLabels
+// recipe.calories
+// recipe.cuisineType
+// recipe.healthLabels
+// recipe.images["REGULAR"]
+// recipe.images["THUMBNAIL"]
+// recipe.ingredientLines
+// recipe.totalTime
 
 // object properties that we are using to display as data (Tasty API)
 // arr[i].name
